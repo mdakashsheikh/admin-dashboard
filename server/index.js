943ea7341e1244382d39f70f9cb50cbb9acbf010
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-03-05 02:08:28 +0600

    addmin dash

---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const clientRoutes = require('./routes/client');
+const generalRoutes = require('./routes/general');
+const managementRoutes = require('./routes/management');
+const salesRoutes = require('./routes/sales');
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES*/
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
+
+const PORT = process.env.PORT || 9000
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log(`Listening on PORT ${PORT}`);
+        console.log('Database Connected!')
+    })
+    .catch((error) => console.log(`${error.message} did not connect`))
